b9b790b9e10c00ddc2790af09611cc5bab5cdb2b
michaeljymsgutierrez <leeez711@gmail.com>
2016-07-20 13:08:24 +0800

    platforms updated

---
+++ platforms/android/build/generated/source/buildConfig/debug/com/ionicframework/w3schoolsoffline643782/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ionicframework.w3schoolsoffline643782;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ionicframework.w3schoolsoffline643782";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 18;
+  public static final String VERSION_NAME = "0.0.1";
+}
